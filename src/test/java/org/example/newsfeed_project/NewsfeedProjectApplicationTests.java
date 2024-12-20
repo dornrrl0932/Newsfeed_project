cb82d67c3c643a6fc83553b2e688cdf31ce5ce08
dornrrl0932 <dornrrl0932@naver.com>
2024-12-20 17:41:40 +0900

    first commit

---
+++ src/test/java/org/example/newsfeed_project/NewsfeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newsfeed_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

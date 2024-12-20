6a9943a4c77a266733711260cfa161345ca9f24d
neulbin <msg0447@gmail.com>
2024-12-20 20:22:57 +0900

    Feat: 포스트 엔티티 추가
    포스트 엔티티를 추가 했습니다.
    postId, contents, title, createdDate, updatedDate

---
+++ src/main/java/org/example/newsfeed_project/entity/Post.java
@@ -0,0 +1,22 @@
+package org.example.newsfeed_project.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "Post")
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    private String contents;
+    private String title;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+}

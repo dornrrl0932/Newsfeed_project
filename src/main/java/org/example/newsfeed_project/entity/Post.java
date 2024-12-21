99483b5a78b99f3f61ee67413576968d53191b3e
duol9 <lhyy0195@gmail.com>
2024-12-21 16:51:52 +0900

    feat: Post 엔티티에 컬럼 제약 조건 및 연관관계 추가
    - title 필드에 길이 제한(@Column(length = 20)) 추가
    - contents 필드에 longtext 타입 지정 (@Column(columnDefinition = "longtext"))
    - createdDate 필드에 @CreatedDate와 수정 불가 옵션(@Column(updatable = false)) 추가
    - updatedDate 필드에 @LastModifiedDate를 적용하여 수정 시 자동 반영되도록 설정
    - User 엔티티와 N:1 관계 설정 (@ManyToOne, @JoinColumn(name = "user_id"))

--- src/main/java/org/example/newsfeed_project/entity/Post.java
+++ src/main/java/org/example/newsfeed_project/entity/Post.java
@@ -2,6 +2,8 @@ package org.example.newsfeed_project.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -14,9 +16,19 @@ public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
-    private String contents;
+    @Column(length = 20)
     private String title;
+    @Column(columnDefinition = "longtext")
+    private String contents;
+    @CreatedDate
+    @Column(updatable = false) // 수정 불가
     private LocalDateTime createdDate;
+    @CreatedDate
+    @LastModifiedDate // 수정 날짜 반영
     private LocalDateTime updatedDate;
+
+    @ManyToOne // post : user -> N:1
+    @JoinColumn(name = "user_id") // user 테이블의 기본키 참조(user_id)
+    private User user;
 
 }

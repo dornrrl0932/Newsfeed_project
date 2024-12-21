307ac34ef3bdc494591a0a7e82578707b272cb1f
duol9 <lhyy0195@gmail.com>
2024-12-22 01:42:13 +0900

    Feat: 유저 조회 시 예외처리로 validateExeption 적용

--- src/main/java/org/example/newsfeed_project/Follow/service/FollowService.java
+++ src/main/java/org/example/newsfeed_project/Follow/service/FollowService.java
@@ -6,9 +6,9 @@ import org.example.newsfeed_project.Follow.repository.FollowRepository;
 import org.example.newsfeed_project.User.repository.UserRepository;
 import org.example.newsfeed_project.entity.Follow;
 import org.example.newsfeed_project.entity.User;
+import org.example.newsfeed_project.exception.ValidateException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +20,10 @@ public class FollowService {
     public FollowDto follow(Long user_id, Long loginUserId){
         // 팔로우 한 유저 조회, 없을 시 404 Not found 반환
         User loginUser = userRepository.findById(loginUserId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않은 회원입니다."));
+                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
         // 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
         User followingUser = userRepository.findById(user_id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않은 회원입니다."));
+                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
 
         Follow follow = new Follow(followingUser, loginUser);
         followRepository.save(follow);

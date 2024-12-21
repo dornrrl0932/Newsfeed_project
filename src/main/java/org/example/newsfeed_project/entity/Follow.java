2693ec58d228393f84a4712fa8325c6269ed1441
duol9 <lhyy0195@gmail.com>
2024-12-21 23:13:17 +0900

    Feat: following/follower setter 생성
    - 팔로잉/팔로워 유저 setting 하기 위해 생성

--- src/main/java/org/example/newsfeed_project/entity/Follow.java
+++ src/main/java/org/example/newsfeed_project/entity/Follow.java
@@ -2,6 +2,7 @@ package org.example.newsfeed_project.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Entity
@@ -11,11 +12,13 @@ public class Follow {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followId;
 
+    @Setter
     @ManyToOne
     @JoinColumn(name = "following_id")
-    private User following;
+    private User following;  // 팔로잉 된 유저
 
+    @Setter
     @ManyToOne
     @JoinColumn(name = "follower_id")
-    private User follower;
+    private User follower; // 팔로우 한 유저 (본인)
 }

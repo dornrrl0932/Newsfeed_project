5c45353e542159f81940652374b36f66e15e3cf4
duol9 <lhyy0195@gmail.com>
2024-12-22 02:52:56 +0900

    Feat: 해당 팔로우 관계 가진 follow 객체 찾는 메서드 구현
    - 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )

--- src/main/java/org/example/newsfeed_project/Follow/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed_project/Follow/repository/FollowRepository.java
@@ -1,7 +1,12 @@
 package org.example.newsfeed_project.Follow.repository;
 
 import org.example.newsfeed_project.entity.Follow;
+import org.example.newsfeed_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    // 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )
+    Optional<Follow> findByFollowingAndFollower(User followingUser, User loginUser);
 }

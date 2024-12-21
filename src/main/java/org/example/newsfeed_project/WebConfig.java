b89ea510d7bf7576ba420c8e37348b7db7356131
임PRO스터\su992 <su9926@naver.com>
2024-12-21 23:17:31 +0900

    Feat: 로그인 필터 추가
    - LoginCheckFilter에 로그인 필터 구현
    - 회원가입, 로그인을 제외한 모든 URI에 로그인 필터 적용
    - WebConfig에 로그인 필터 등록
    - SessionConst에 session에서 사용할 식별자 추가

---
+++ src/main/java/org/example/newsfeed_project/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.newsfeed_project;
+
+import org.example.newsfeed_project.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import jakarta.servlet.Filter;
+
+public class WebConfig {
+	/*로그인 상태 체크 필터 등록*/
+	@Bean
+	public FilterRegistrationBean loginCheckFilter() {
+
+		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+		filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+		filterFilterRegistrationBean.setOrder(1);
+		filterFilterRegistrationBean.addUrlPatterns("/*");
+
+		return filterFilterRegistrationBean;
+	}
+}
---
+++ src/main/java/org/example/newsfeed_project/session/SessionConst.java
@@ -0,0 +1,6 @@
+package org.example.newsfeed_project.session;
+
+/*세션 테이블 식별자(Key)*/
+public class SessionConst {
+	public static final String LOGIN_USER = "loginUser";
+}

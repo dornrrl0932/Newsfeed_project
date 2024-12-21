d08c6ab12f7fcb6390e97db1087545839857534a
duol9 <lhyy0195@gmail.com>
2024-12-22 01:41:10 +0900

    Feat: 예외처리 클래스 생성
    ValidateException 클래스 생성
    - HttpSatus와 메시지 포함
    GlobalExceptionHandler에 ValidateException 처리 로직 추가
    - 발생한 예외의 메시지와 상태코드 반환

--- src/main/java/org/example/newsfeed_project/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/newsfeed_project/exception/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package org.example.newsfeed_project.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice // 컨트롤러 전역에 활성화
+// 예외 관리
 public class GlobalExceptionHandler {
+
+    // ValidateException 처리
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity<String> handleValidateException(ValidateException exception) {
+        return new ResponseEntity<>(exception.getMessage(), exception.getHttpStatus());
+    }
 }

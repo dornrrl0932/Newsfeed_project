563142ee6e5a3d362f284a21813aaa4a194fe05c
duol9 <lhyy0195@gmail.com>
2024-12-22 02:54:38 +0900

    Feat: 팔로우 취소 비즈니스 로직 추가
    - 팔로우 한/당한 유저 조회, 없을 시 예외처리 (404)
    - 팔로우 되어 있는지 확인, 없을 시 예외처리 (400)
    - 팔로우 관계 삭제 (해당 row 삭제)

--- src/main/java/org/example/newsfeed_project/Follow/service/FollowService.java
+++ src/main/java/org/example/newsfeed_project/Follow/service/FollowService.java
@@ -30,4 +30,23 @@ public class FollowService {
 
         return new FollowDto(followingUser.getUserName() + "님을 팔로우 했습니다.");
     }
+
+    // 언팔로우(팔로우 취소)
+    public FollowDto unFollow(Long user_id, Long loginUserId){
+        // 팔로우 한 유저 조회, 없을 시 404 Not found 반환
+        User loginUser = userRepository.findById(loginUserId)
+                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+        // 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
+        User followingUser = userRepository.findById(user_id)
+                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+
+        // 팔로우 되어 있는지 확인
+        Follow follow = followRepository.findByFollowingAndFollower(followingUser, loginUser)
+                .orElseThrow(() -> new ValidateException("팔로잉 되어 있지 않은 회원입니다.", HttpStatus.BAD_REQUEST));
+
+        // 팔로우 관계 삭제
+        followRepository.delete(follow);
+
+        return new FollowDto(followingUser.getUserName() + "님을 팔로우 취소했습니다.");
+    }
 }

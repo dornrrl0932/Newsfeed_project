dee50cd5eb1e409eb882a08a4242243f7c9c9af0
duol9 <lhyy0195@gmail.com>
2024-12-22 00:02:14 +0900

    Feat: Follow 생성자 추가
    - 일관성 유지를 위해 setter 없애고 생성자 추가

--- src/main/java/org/example/newsfeed_project/entity/Follow.java
+++ src/main/java/org/example/newsfeed_project/entity/Follow.java
@@ -12,13 +12,18 @@ public class Follow {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followId;
 
-    @Setter
     @ManyToOne
     @JoinColumn(name = "following_id")
     private User following;  // 팔로잉 된 유저
 
-    @Setter
     @ManyToOne
     @JoinColumn(name = "follower_id")
     private User follower; // 팔로우 한 유저 (본인)
+
+    public Follow(User follower, User following){
+        this.following = following;
+        this.follower = follower;
+    }
+
+    public Follow() {}
 }

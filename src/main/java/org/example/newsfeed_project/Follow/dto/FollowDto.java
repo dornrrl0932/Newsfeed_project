c8476e71b56f61bafb6babdcb263398d1afd1a2f
duol9 <lhyy0195@gmail.com>
2024-12-21 23:10:30 +0900

    Feat: FollowDto 클래스 추가
    - FollowDto 클래스 생성
    - 팔로우 요청에 대한 응답 메시지 전달

---
+++ src/main/java/org/example/newsfeed_project/Follow/dto/FollowDto.java
@@ -0,0 +1,10 @@
+package org.example.newsfeed_project.Follow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FollowDto {
+    private String message;
+}

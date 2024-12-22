ec36dbf1a8a959f9481954654c169bc72af0f664
duol9 <lhyy0195@gmail.com>
2024-12-22 18:08:18 +0900

    Feat: 팔로우/팔로잉 수 반환하는 메서드 추가
    유저(user_id)를 참조해 follow 테이블 조회 후 개수를 반환

--- src/main/java/org/example/newsfeed_project/Follow/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed_project/Follow/repository/FollowRepository.java
@@ -9,4 +9,8 @@ import java.util.Optional;
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     // 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )
     Optional<Follow> findByFollowingAndFollower(User followingUser, User loginUser);
+    // 팔로잉 수
+    Long countByFollowing(User user);
+    // 팔로워 수
+    Long countByFollower(User user);
 }

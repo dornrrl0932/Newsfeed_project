be1d3e3a1bf94458f5b77bf6a0418c0efea5fd5a
duol9 <lhyy0195@gmail.com>
2024-12-22 21:38:56 +0900

    Feat: PostPageDto 정적 팩토리 메서드 추가
    - Page<Post> -> List<PostPageDto>로 변환 (Entity -> Dto)

--- src/main/java/org/example/newsfeed_project/post/dto/PostPageDto.java
+++ src/main/java/org/example/newsfeed_project/post/dto/PostPageDto.java
@@ -2,7 +2,13 @@ package org.example.newsfeed_project.post.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.example.newsfeed_project.entity.Post;
+import org.springframework.data.domain.Page;
+
+import java.security.cert.CertPath;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -11,4 +17,16 @@ public class PostPageDto {
     private String title;
     private String contents;
     private LocalDateTime updateDate;
+
+    // Page<Post> -> List<PostPageDto> 로 변환 (엔티티 -> Dto)
+    public static List<PostPageDto> convertFrom(Page<Post> postPage) {
+        return postPage.getContent().stream()
+                .map(post -> new PostPageDto(
+                        post.getUser().getUserName(),
+                        post.getTitle(),
+                        post.getContents(),
+                        post.getUpdatedDate()
+                ))
+                .collect(Collectors.toList());
+    }
 }

84c33bd4dc7317446df9257f2d7afd8d4637b68d
duol9 <lhyy0195@gmail.com>
2024-12-22 18:10:01 +0900

    Feat: 게시글 페이지네이션 구현
    - 특정 유저의 게시물 10개 불러오는 쿼리 메서드 구현
    - 수정일 기준 내림차순 정렬

---
+++ src/main/java/org/example/newsfeed_project/post/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package org.example.newsfeed_project.post.repository;
+
+import org.example.newsfeed_project.entity.Post;
+import org.example.newsfeed_project.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    /**
+     * 특정 유저의 게시물 10개 불러오기 (Post 타입으로 저장)
+     *     @Query (" SELECT p " +
+     *             "FROM Post p " +
+     *             "JOIN p.user u " +
+     *             "WHERE u = :user " +
+     *             "ORDER BY p.updatedDate DESC"
+     *     )
+     */
+    Page<Post> findByUserOrderByUpdatedDateDesc(User user, Pageable pageable);
+}

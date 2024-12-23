8efb0f97f9f13fae2f02209297a6c004a4bc9a37
duol9 <lhyy0195@gmail.com>
2024-12-24 01:40:43 +0900

    Feat: 팔로우 요청 승인 기능 추가
    - 새 관계 생성 후 저장, 승인 상태 업데이트 등 여러 DB 작업이 존재하므로 트랜잭션 추가 (예외 발생 시 롤백)
    - 로그인 유저 -> 팔로잉 한 유저 의 관계가 follow 엔티티에 존재하는지 확인
      - 존재 확인하는 메서드 추가
    - 존재 여부에 따라 follow 관계 생성 시 승인/요청 선택이 달라짐
    - 존재 시 팔로잉 한 유저 -> 로그인 유저 팔로우 승인(true)으로 업데이트 후 저장

--- src/main/java/org/example/newsfeed_project/follow/service/FollowService.java
+++ src/main/java/org/example/newsfeed_project/follow/service/FollowService.java
@@ -1,56 +1,82 @@
 package org.example.newsfeed_project.follow.service;
 
-import lombok.RequiredArgsConstructor;
-import org.example.newsfeed_project.follow.dto.MessageDto;
-import org.example.newsfeed_project.follow.repository.FollowRepository;
-import org.example.newsfeed_project.user.repository.UserRepository;
 import org.example.newsfeed_project.entity.Follow;
 import org.example.newsfeed_project.entity.User;
 import org.example.newsfeed_project.exception.ValidateException;
+import org.example.newsfeed_project.follow.dto.MessageDto;
+import org.example.newsfeed_project.follow.repository.FollowRepository;
+import org.example.newsfeed_project.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class FollowService {
-    private final FollowRepository followRepository;
-    private final UserRepository userRepository;
+	private final FollowRepository followRepository;
+	private final UserRepository userRepository;
 
-    // 팔로우하기
-    public MessageDto follow(Long user_id, Long loginUserId){
-        // 팔로우 한 유저 조회, 없을 시 404 Not found 반환
-        User loginUser = userRepository.findById(loginUserId)
-                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
-        // 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
-        User followingUser = userRepository.findById(user_id)
-                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+	// 팔로우하기
+	@Transactional(rollbackFor = Exception.class)
+	public MessageDto follow(Long user_id, Long loginUserId) {
+		// 팔로우 한 유저 조회, 없을 시 404 Not found 반환
+		User loginUser = userRepository.findById(loginUserId)
+			.orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+		// 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
+		User followingUser = userRepository.findById(user_id)
+			.orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
 
-        Follow follow = Follow.builder()
-                .follower(loginUser)
-                .following(followingUser)
-                .isApprove(false) // 처음엔 요청만 넣은 상태
-                .build();
-        followRepository.save(follow);
+		// 이미 팔로우 중인지 확인
+		if (followRepository.existsByFollowerAndFollowing(loginUser, followingUser)) {
+			throw new ValidateException((followingUser.getUserName() + "님은 이미 팔로우 중입니다."), HttpStatus.CONFLICT);
+		}
 
-        return new MessageDto(followingUser.getUserName() + "님을 팔로우 했습니다.");
-    }
+		// (로그인 유저가) 팔로우 한 유저 -> 로그인 유저 팔로잉 했는지 확인 (들어온 요청이 있는지 확인)
+		boolean isExitstingFollow = followRepository.existsByFollowerAndFollowing(followingUser, loginUser);
 
-    // 언팔로우(팔로우 취소)
-    public MessageDto unFollow(Long user_id, Long loginUserId){
-        // 팔로우 한 유저 조회, 없을 시 404 Not found 반환
-        User loginUser = userRepository.findById(loginUserId)
-                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
-        // 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
-        User followingUser = userRepository.findById(user_id)
-                .orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+		// 팔로우 관계 생성
+		Follow follow = Follow.builder()
+			.follower(loginUser)
+			.following(followingUser)
+			.isApprove(isExitstingFollow) // 존재하면 true(승인), 아니면 false(요청)
+			.build();
+		followRepository.save(follow);
 
-        // 팔로우 되어 있는지 확인
-        Follow follow = followRepository.findByFollowingAndFollower(followingUser, loginUser)
-                .orElseThrow(() -> new ValidateException("팔로잉 되어 있지 않은 회원입니다.", HttpStatus.BAD_REQUEST));
+		// 존재하면 요청에 대한 승인
+		if (isExitstingFollow) {
+			// (로그인 유저가) 팔로잉 한 유저 -> 로그인 한 유저 관계
+			Follow followingUserFollow = findByFollowRelation(followingUser, loginUser);
+			followingUserFollow.setIsApprove(true); // 요청이 승인 되었으므로 true
+			followRepository.save(followingUserFollow); // 저장
+		}
 
-        // 팔로우 관계 삭제
-        followRepository.delete(follow);
+		return new MessageDto(followingUser.getUserName() + "님을 팔로우 했습니다.");
+	}
 
-        return new MessageDto(followingUser.getUserName() + "님을 팔로우 취소했습니다.");
-    }
+	// 언팔로우(팔로우 취소)
+	public MessageDto unFollow(Long user_id, Long loginUserId) {
+		// 팔로우 한 유저 조회, 없을 시 404 Not found 반환
+		User loginUser = userRepository.findById(loginUserId)
+			.orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+		// 팔로우 당한 유저 조회, 없을 시 404 Not found 반환
+		User followingUser = userRepository.findById(user_id)
+			.orElseThrow(() -> new ValidateException("존재하지 않은 회원입니다.", HttpStatus.NOT_FOUND));
+
+		// 팔로우 되어 있는지 확인
+		Follow follow = findByFollowRelation(followingUser, loginUser);
+
+		// 팔로우 관계 삭제
+		followRepository.delete(follow);
+
+		return new MessageDto(followingUser.getUserName() + "님을 팔로우 취소했습니다.");
+	}
+
+	// 팔로우 관계 조회
+	public Follow findByFollowRelation(User loginUser, User followingUser) {
+		// 팔로우 되어 있는지 확인
+		return followRepository.findByFollowerAndFollowing(loginUser, followingUser)
+			.orElseThrow(() -> new ValidateException("팔로잉 되어 있지 않은 회원입니다.", HttpStatus.BAD_REQUEST));
+	}
 }

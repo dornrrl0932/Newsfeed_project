3709e97b7a7392974692bc45f2e3cd31bdc52e1d
임PRO스터\su992 <su9926@naver.com>
2024-12-23 20:32:53 +0900

    Fix: @Configuration 추가
    - WebConfig에 @Configuration 애너테이션 추가

--- src/main/java/org/example/newsfeed_project/WebConfig.java
+++ src/main/java/org/example/newsfeed_project/WebConfig.java
@@ -3,9 +3,11 @@ package org.example.newsfeed_project;
 import org.example.newsfeed_project.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import jakarta.servlet.Filter;
 
+@Configuration
 public class WebConfig {
 	/*로그인 상태 체크 필터 등록*/
 	@Bean

82f8e7338dd4b539eb851fc2d2aabc0747e8eb0c
duol9 <lhyy0195@gmail.com>
2024-12-23 15:03:02 +0900

    Rename: 생성/수정일 변수명 변경
    - 기존 updatedDate는 날짜, 시간을 나타내는 LocalDateTime 타입의 의도와 맞지 않다고 판단
    - 날짜+시간을 포함한 것을 알 수 있는 변수명으로 변경
    - createdDate -> createdAt
    - updatedDate -> updatedAt

--- src/main/java/org/example/newsfeed_project/entity/Post.java
+++ src/main/java/org/example/newsfeed_project/entity/Post.java
@@ -22,10 +22,10 @@ public class Post {
     private String contents;
     @CreatedDate
     @Column(updatable = false) // 수정 불가
-    private LocalDateTime createdDate;
+    private LocalDateTime createdAt;
     @CreatedDate
     @LastModifiedDate // 수정 날짜 반영
-    private LocalDateTime updatedDate;
+    private LocalDateTime updatedAt;
 
     @ManyToOne // post : user -> N:1
     @JoinColumn(name = "user_id") // user 테이블의 기본키 참조(user_id)

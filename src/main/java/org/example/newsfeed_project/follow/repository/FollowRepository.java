63c14a79fc5d96cfd3a8843ca35daf36d1df65b2
duol9 <lhyy0195@gmail.com>
2024-12-24 01:15:31 +0900

    Feat: 팔로우 관계 확인하는 쿼리메서드 추가
    - 특정 유저가 팔로워를 팔로잉 중인지 확인 할 때 사용(또는 그 반대)
    - 관계가 존재하는지 확인 가능

--- src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
@@ -1,16 +1,21 @@
 package org.example.newsfeed_project.follow.repository;
 
+import java.util.Optional;
+
 import org.example.newsfeed_project.entity.Follow;
 import org.example.newsfeed_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-    // 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )
-    Optional<Follow> findByFollowingAndFollower(User followingUser, User loginUser);
-    // 팔로잉 수
-    Long countByFollowing(User user);
-    // 팔로워 수
-    Long countByFollower(User user);
+	// 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )
+	Optional<Follow> findByFollowerAndFollowing(User followerUser, User followingUser);
+
+	// 팔로잉 수
+	Long countByFollowing(User user);
+
+	// 팔로워 수
+	Long countByFollower(User user);
+
+	// 팔로우 관계가 존재하는지 확인
+	Boolean existsByFollowerAndFollowing(User followerUser, User followingUser);
 }

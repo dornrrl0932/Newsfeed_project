9a500079c9a88cf2b8251b74a54114105fe03462
임PRO스터\su992 <su9926@naver.com>
2024-12-23 16:30:13 +0900

    Feat: 로그인 API 추가
    - 로그인 API 추가함

--- src/main/java/org/example/newsfeed_project/controller/UserController.java
+++ src/main/java/org/example/newsfeed_project/controller/UserController.java
@@ -1,10 +1,17 @@
 package org.example.newsfeed_project.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+
+import org.example.newsfeed_project.dto.LoginRequestDto;
 import org.example.newsfeed_project.dto.SignUpRequestDto;
+import org.example.newsfeed_project.entity.User;
 import org.example.newsfeed_project.service.UserService;
+import org.example.newsfeed_project.session.SessionConst;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    //회원가입
-    @PostMapping("signup")
-    public ResponseEntity<String> signupUser (@RequestBody SignUpRequestDto signUpRequestDto) {
+	//회원가입
+	@PostMapping("signup")
+	public ResponseEntity<String> signupUser(@RequestBody SignUpRequestDto signUpRequestDto) {
 
-        userService.signupUser(signUpRequestDto);
+		userService.signupUser(signUpRequestDto);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
-    }
+		return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
+	}
+
+	//로그인
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@Validated @RequestBody LoginRequestDto loginRequestDto,
+		HttpServletRequest request) {
+
+		User loginUser = userService.login(loginRequestDto);
+
+		//로그인 성공 처리
+		HttpSession session = request.getSession();
+		session.setAttribute(SessionConst.LOGIN_USER_NAME, loginUser.getUserName());
+		session.setAttribute(SessionConst.LOGIN_USER_ID, loginUser.getUserId());
+		session.setAttribute(SessionConst.USER_STATUS, loginUser.getStatus());
+
+		return ResponseEntity.status(HttpStatus.OK).body("로그인 되었습니다.");
+	}
 
 }
--- src/main/java/org/example/newsfeed_project/repository/UserRepository.java
+++ src/main/java/org/example/newsfeed_project/repository/UserRepository.java
@@ -1,7 +1,19 @@
 package org.example.newsfeed_project.repository;
 
+import java.util.Optional;
+
 import org.example.newsfeed_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findUserByEmail(String email);
+
+	default User findUserByEmailOrElseThrow(String email) {
+		return findUserByEmail(email)
+			.orElseThrow(() -> new ResponseStatusException(
+				HttpStatus.UNAUTHORIZED, "Dose not exist email: " + email));
+	}
 }

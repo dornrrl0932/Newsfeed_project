a9bde5364f152b501e5142c49a0b3a80c1f85138
dornrrl0932 <dornrrl0932@naver.com>
2024-12-24 14:06:51 +0900

    Feat: Passwordauthenticationexception 클래스 생성
    - 비밀번호 확인 예외처리를 위해 생성

--- src/main/java/org/example/newsfeed_project/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/newsfeed_project/exception/GlobalExceptionHandler.java
@@ -8,20 +8,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //탈퇴 처리 된 회원 예외처리
-    @ExceptionHandler(UserDeletedException.class)
-    public ResponseEntity<String> handleUserDeletedException(UserDeletedException e) {
+	//탈퇴 처리 된 회원 예외처리
+	@ExceptionHandler(UserDeletedException.class)
+	public ResponseEntity<String> handleUserDeletedException(UserDeletedException e) {
 
-        //400 BAD_REQUEST와 메세지를 반환
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+		//400 BAD_REQUEST와 메세지를 반환
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
 
-    //잘못 된 URL에 대한 예외처리
-    @ExceptionHandler(InvalidUrlException.class)
-    public ResponseEntity<String> handleInvalidUrlException(InvalidUrlException e) {
+	//잘못 된 URL에 대한 예외처리
+	@ExceptionHandler(InvalidUrlException.class)
+	public ResponseEntity<String> handleInvalidUrlException(InvalidUrlException e) {
 
-        //404 Not_Found와 메세지를 반환
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("잘못 된 URL입니다. 다시 입력해주세요.");
-    }
+		//404 Not_Found와 메세지를 반환
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("잘못 된 URL입니다. 다시 입력해주세요.");
+	}
+
+	//확인 비밀번호 불일치 예외처리
+	@ExceptionHandler(Passwordauthenticationexception.class)
+	public ResponseEntity<String> HandlePasswordauthenticationexception(Passwordauthenticationexception e) {
+
+		// 401 UNAUTHORIZED 메세지를 반환
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+	}
+
 }
 
---
+++ src/main/java/org/example/newsfeed_project/exception/Passwordauthenticationexception.java
@@ -0,0 +1,8 @@
+package org.example.newsfeed_project.exception;
+
+public class Passwordauthenticationexception extends RuntimeException {
+
+	public Passwordauthenticationexception(String message) {
+		super(message);
+	}
+}

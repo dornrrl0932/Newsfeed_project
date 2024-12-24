5bb36bbb43dbb421e71d79952cc382f3cae68807
duol9 <lhyy0195@gmail.com>
2024-12-24 17:51:17 +0900

    Merge : Comment API 기능 구현 기본 토대 생성

---
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -0,0 +1,21 @@
+package org.example.newsfeed_project.comment.service;
+
+import org.example.newsfeed_project.comment.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+	private CommentRepository commentRepository;
+
+	// 댓글 작성
+
+	// 댓글 조회
+
+	// 댓글 수정
+
+	// 댓글 삭제
+}

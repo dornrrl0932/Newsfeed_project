5f8c504b5d7e50dc2203d538d645568eb07a0909
dornrrl0932 <dornrrl0932@naver.com>
2024-12-24 16:12:17 +0900

    Refactor: 예외처리 수정
    - 기존 코드에서 커스텀 예외 생성 후 적용

--- src/main/java/org/example/newsfeed_project/user/controller/UserController.java
+++ src/main/java/org/example/newsfeed_project/user/controller/UserController.java
@@ -1,47 +1,53 @@
 package org.example.newsfeed_project.user.controller;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.example.newsfeed_project.exception.InvalidUrlException;
 import org.example.newsfeed_project.user.dto.CancelRequestDto;
 import org.example.newsfeed_project.user.dto.SignUpRequestDto;
-import org.example.newsfeed_project.exception.InvalidUrlException;
 import org.example.newsfeed_project.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    //회원가입
-    @PostMapping("signup")
-    public ResponseEntity<String> signupUser (@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+	//회원가입
+	@PostMapping("signup")
+	public ResponseEntity<String> signupUser(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
 
-        userService.signupUser(signUpRequestDto);
+		userService.signupUser(signUpRequestDto);
 
-        //회원가입 완료 시 CREATED 상태코드와 메세지 반환
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
-    }
+		//회원가입 완료 시 CREATED 상태코드와 메세지 반환
+		return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
+	}
 
+	//잘못 된 경로 예외처리
+	@RequestMapping("*")
+	public void handleInvalidUrl() {
+		//InvalidUrlException를 통해 예외처리
+		throw new InvalidUrlException("잘못 된 URL입니다. 다시 입력해주세요.");
+	}
 
-    //잘못 된 경로 예외처리
-    @RequestMapping("*")
-    public void handleInvalidUrl() {
-        //InvalidUrlException를 통해 예외처리
-        throw new InvalidUrlException("잘못 된 URL입니다. 다시 입력해주세요.");
-    }
+	//회원탈퇴
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<String> CancelUser(@PathVariable Long userId,
+		@RequestBody CancelRequestDto cancelRequestDto) {
 
-    //회원탈퇴
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<String> CancelUser (@PathVariable Long userId, @RequestBody CancelRequestDto cancelRequestDto) {
+		userService.CancelUser(userId, cancelRequestDto);
 
-        userService.CancelUser(userId, cancelRequestDto);
-
-        //탈퇴 성공 시 OK 상태코드와 메세지 반환
-        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 탈퇴 되었습니다. 감사합니다.");
-    }
+		//탈퇴 성공 시 OK 상태코드와 메세지 반환
+		return ResponseEntity.status(HttpStatus.OK).body("정상적으로 탈퇴 되었습니다. 감사합니다.");
+	}
 }
--- src/main/java/org/example/newsfeed_project/user/service/UserService.java
+++ src/main/java/org/example/newsfeed_project/user/service/UserService.java
@@ -1,72 +1,70 @@
 package org.example.newsfeed_project.user.service;
 
-import lombok.RequiredArgsConstructor;
+import org.example.newsfeed_project.entity.User;
+import org.example.newsfeed_project.exception.EmailAlreadyExistsException;
+import org.example.newsfeed_project.exception.PasswordAuthenticationException;
+import org.example.newsfeed_project.exception.UserDeletedException;
 import org.example.newsfeed_project.user.config.PasswordEncoder;
 import org.example.newsfeed_project.user.dto.CancelRequestDto;
 import org.example.newsfeed_project.user.dto.SignUpRequestDto;
-import org.example.newsfeed_project.entity.User;
-import org.example.newsfeed_project.exception.InvalidUrlException;
-import org.example.newsfeed_project.exception.UserDeletedException;
 import org.example.newsfeed_project.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
+	//회원 가입
+	public void signupUser(SignUpRequestDto signUpRequestDto) {
 
-    //회원 가입
-    public void signupUser(SignUpRequestDto signUpRequestDto) {
+		//비밀번호 확인 : 입력한 비밀번호와 확인 비밀번호가 일치하는지 확인
+		if (!signUpRequestDto.getPassword().equals(signUpRequestDto.getRenterPassword())) {
+			throw new PasswordAuthenticationException("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
+		}
 
-        InvalidUrlException e = new InvalidUrlException("잘못 된 URL 입니다");
+		//이메일 중복 확인
+		if (userRepository.findByEmail(signUpRequestDto.getEmail()).isPresent()) {
+			throw new EmailAlreadyExistsException("이미 사용중인 이메일입니다.");
+		}
 
-        //비밀번호 확인 : 입력한 비밀번호와 확인 비밀번호가 일치하는지 확인
-        if (!signUpRequestDto.getPassword().equals(signUpRequestDto.getRenterPassword())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호와 확인 비밀번호가 일치하지 않습니다.");
-        }
+		//비밀번호 암호화
+		String encodedPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
 
-        //이메일 중복 확인
-        if (userRepository.findByEmail(signUpRequestDto.getEmail()).isPresent()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다.");
-        }
+		//회원 생성
+		User user = new User(signUpRequestDto.getEmail(), encodedPassword, signUpRequestDto.getUserName());
 
-        //비밀번호 암호화
-        String encodedPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
+		//DB에 저장
+		userRepository.save(user);
+	}
 
-        //회원 생성
-        User user = new User(signUpRequestDto.getEmail(), encodedPassword, signUpRequestDto.getUserName());
+	//회원 탈퇴
+	public void CancelUser(Long userId, CancelRequestDto cancelRequestDto) {
 
-        //DB에 저장
-        userRepository.save(user);
-    }
+		//userId로 회원 검색, 일치하는 회원이 없는 경우 NOT_FOUND 반환
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID가 존재하지 않습니다. = " + userId));
 
+		//비밀번호 확인 : 입력한 비밀번호와 확인 비밀번호가 일치하는지 확인
+		if (!cancelRequestDto.getPassword().equals(cancelRequestDto.getRenterPassword())) {
+			throw new PasswordAuthenticationException("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
+		}
 
-    //회원 탈퇴
-    public void CancelUser(Long userId, CancelRequestDto cancelRequestDto) {
+		//탈퇴처리된 회원 예외처리 -> status가 false인 경우 에러 반환
+		if (!user.getStatus()) {
+			throw new UserDeletedException("이미 탈퇴 처리 된 회원입니다.");
+		}
 
-        //userId로 회원 검색, 일치하는 회원이 없는 경우 NOT_FOUND 반환
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID가 존재하지 않습니다. = " + userId));
+		//탈퇴처리 -> 회원 상태 false로 변경
+		user.setStatus(false);
 
-        //비밀번호 확인 : 입력한 비밀번호와 확인 비밀번호가 일치하는지 확인
-        if (!cancelRequestDto.getPassword().equals(cancelRequestDto.getRenterPassword())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호와 확인 비밀번호가 일치하지 않습니다.");
-        }
-
-        //탈퇴처리된 회원 예외처리 -> status가 false인 경우 에러 반환
-        if(!user.getStatus()) {
-            throw new UserDeletedException("이미 탈퇴 처리 된 회원입니다.");
-        }
-
-        //탈퇴처리 -> 회원 상태 false로 변경
-        user.setStatus(false);
-
-        //deleteById는 하드 삭제를 수행 -> save로 현재 상태를 업데이트
-        userRepository.save(user);
-    }
+		//deleteById는 하드 삭제를 수행 -> save로 현재 상태를 업데이트
+		userRepository.save(user);
+	}
 }

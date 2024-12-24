e8efc433db04aca9cbb4bc2654ed002b235a1f34
duol9 <lhyy0195@gmail.com>
2024-12-24 16:35:29 +0900

    Feat: 팔로잉 목록 조회 JPQL 구현
    - isApprove = true면 status = '맞팔로우', isApprove = false면 status = '요청 중'
    - 팔로워_id가 user인 row 조회

--- src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
@@ -32,4 +32,14 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 		"WHERE f.following = :user" // 팔로잉 유저 파라미터 user와 같은 것들만
 	)
 	List<FollowUserInfoDto> findFollowersInfoByUser(@Param("user") User user);
+
+	// 팔로잉 정보 조회 (follower_id가 user_id인 row 조회)
+	@Query("SELECT new org.example.newsfeed_project.follow.dto.FollowUserInfoDto(" + // dto 생성
+		"u.userName, u.introduction, " +
+		"CASE WHEN f.isApprove = true THEN '맞팔로우' ELSE '요청 중' END) " +
+		"FROM Follow f " +
+		"JOIN f.following u " +  // follower_id와 연관된 유저 정보 가져옴
+		"WHERE f.follower = :user" // 팔로잉 유저 파라미터 user와 같은 것들만
+	)
+	List<FollowUserInfoDto> findFollowingsInfoByUser(@Param("user") User user);
 }

4be6b0d11e3fe4fae043186421866254230c1496
duol9 <lhyy0195@gmail.com>
2024-12-24 15:43:03 +0900

    Feat: 팔로워 목록 조회 JPQL 구현
    - 입력 들어온 유저를 팔로잉 중인 팔로워들을 조회
    - FollowUserInfoDto 형태로 조회 (유저명, 소개말, 팔로우 상태)

--- src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed_project/follow/repository/FollowRepository.java
@@ -1,10 +1,14 @@
 package org.example.newsfeed_project.follow.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.example.newsfeed_project.entity.Follow;
 import org.example.newsfeed_project.entity.User;
+import org.example.newsfeed_project.follow.dto.FollowUserInfoDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 	// 팔로우 관계 없을 경우를 대비해 Optional<> 사용 ( .orElseThrow() )
@@ -18,4 +22,14 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 
 	// 팔로우 관계가 존재하는지 확인
 	Boolean existsByFollowerAndFollowing(User followerUser, User followingUser);
+
+	// 팔로워 정보 조회 (following_id가 user_id인 row 조회)
+	@Query("SELECT new org.example.newsfeed_project.follow.dto.FollowUserInfoDto(" + // dto 생성
+		"u.userName, u.introduction, " +
+		"CASE WHEN f.isApprove = true THEN '맞팔로우' ELSE '승인 전' END) " +
+		"FROM Follow f " +
+		"JOIN f.follower u " +  // follower_id와 연관된 유저 정보 가져옴
+		"WHERE f.following = :user" // 팔로잉 유저 파라미터 user와 같은 것들만
+	)
+	List<FollowUserInfoDto> findFollowersInfoByUser(@Param("user") User user);
 }

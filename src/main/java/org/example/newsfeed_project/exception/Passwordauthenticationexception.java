5db49d063bc7a5749157e92bd15eb4c1f0b96844
dornrrl0932 <dornrrl0932@naver.com>
2024-12-24 16:04:14 +0900

    Rename:PasswordAuthenticationException 이름 변경

--- src/main/java/org/example/newsfeed_project/exception/Passwordauthenticationexception.java
+++
@@ -1,8 +0,0 @@
-package org.example.newsfeed_project.exception;
-
-public class Passwordauthenticationexception extends RuntimeException {
-
-	public Passwordauthenticationexception(String message) {
-		super(message);
-	}
-}

c439753b83a4dc07abbd242049d6a7f12781ba87
duol9 <lhyy0195@gmail.com>
2024-12-24 16:01:31 +0900

    Feat: 팔로워 목록 조회 controller 설계
    - 조회 성공 시 200OK
    - FollowerDto 전달

--- src/main/java/org/example/newsfeed_project/follow/controller/FollowController.java
+++ src/main/java/org/example/newsfeed_project/follow/controller/FollowController.java
@@ -1,10 +1,12 @@
 package org.example.newsfeed_project.follow.controller;
 
+import org.example.newsfeed_project.follow.dto.FollowersDto;
 import org.example.newsfeed_project.follow.dto.MessageDto;
 import org.example.newsfeed_project.follow.service.FollowService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,17 @@ public class FollowController {
 		Long loginUserId = 2L; // 로그인 기능 구현 후 수정 필요
 		return new ResponseEntity<>(followService.follow(user_id, loginUserId), HttpStatus.OK);
 	}
-    
+
 	// 팔로우 취소
 	@DeleteMapping("/{user_id}")
 	public ResponseEntity<MessageDto> unFollow(@PathVariable Long user_id) {
 		Long loginUserId = 2L; // 로그인 기능 구현 후 수정 필요
 		return new ResponseEntity<>(followService.unFollow(user_id, loginUserId), HttpStatus.OK);
+	}
+
+	// 팔로워 목록
+	@GetMapping("/{user_id}/followers")
+	public ResponseEntity<FollowersDto> getFollowers(@PathVariable Long user_id) {
+		return ResponseEntity.ok(followService.getFollowers(user_id));
 	}
 }

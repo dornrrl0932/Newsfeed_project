5e4c7583d639c7115b8f7a4b306a51dee4bacfc9
duol9 <lhyy0195@gmail.com>
2024-12-25 17:02:46 +0900

    Feat: 댓글 수정 컨트롤러 구현
    - 세션에서 로그인 유저의 Id 정보 얻어옴
    - 수정 성공 시 응답 코드 200 OK

--- src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
@@ -1,8 +1,17 @@
 package org.example.newsfeed_project.comment.controller;
 
+import org.example.newsfeed_project.comment.dto.CommentDto;
+import org.example.newsfeed_project.comment.service.CommentService;
+import org.example.newsfeed_project.user.session.SessionConst;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -10,11 +19,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentController {
 
+	private final CommentService commentService;
+
 	// 댓글 작성
 
 	// 댓글 조회
 
 	// 댓글 수정
+	@PatchMapping("/{comment_id}")
+	public ResponseEntity<CommentDto> modifyComment(@PathVariable(name = "post_id") Long postId,
+		@PathVariable(name = "comment_id") Long commentId,
+		@RequestBody CommentRequestDto dto, HttpServletRequest servletRequest) {
+
+		HttpSession httpSession = servletRequest.getSession(false);
+		Long loginUserId = (Long)servletRequest.getAttribute(SessionConst.LOGIN_USER_ID);
+
+		return ResponseEntity.ok(commentService.modifyComment(loginUserId, postId, commentId, dto));
+	}
 
 	// 댓글 삭제
 

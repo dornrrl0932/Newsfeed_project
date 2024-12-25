766831c25ed25df0ee07b009f34f38a7c24dcc43
임PRO스터\su992 <su9926@naver.com>
2024-12-26 01:12:39 +0900

    Feat: PostController 추가
    - 게시글 좋아요 상태 토글 API 추가
    - 게시물 단건 조회 API return 부분 수정

--- src/main/java/org/example/newsfeed_project/post/controller/PostController.java
+++ src/main/java/org/example/newsfeed_project/post/controller/PostController.java
@@ -1,15 +1,19 @@
 package org.example.newsfeed_project.post.controller;
 
+import org.example.newsfeed_project.entity.Post;
+import org.example.newsfeed_project.post.dto.LikeNumResponseDto;
 import org.example.newsfeed_project.post.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+
 import org.example.newsfeed_project.post.dto.PostFindByPageRequestDto;
 import org.example.newsfeed_project.post.dto.PostFindByPageResponseDto;
 import org.example.newsfeed_project.post.dto.PostFindDetailByIdResponseDto;
 
-import org.example.newsfeed_project.post.service.PostService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +22,30 @@ import java.util.List;
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
-    private final PostService postService;
+	private final PostService postService;
 
-    @GetMapping("/page/{page}")
-    public List<PostFindByPageResponseDto> findPostByPage(@PathVariable Long page, @RequestBody PostFindByPageRequestDto requestDto) {
-        Long pageSize = 10L;
-        return postService.findPostByPage(page, pageSize, requestDto).get(page);
-    }
+	//게시물 전체 조회
+	@GetMapping("/page/{page}")
+	public List<PostFindByPageResponseDto> findPostByPage(@PathVariable Long page,
+		@RequestBody PostFindByPageRequestDto requestDto) {
+		Long pageSize = 10L;
+		return postService.findPostByPage(page, pageSize, requestDto).get(page);
+	}
 
-    @GetMapping("/{post_id}")
-    public PostFindDetailByIdResponseDto findPostById(@PathVariable Long post_id){
-        return findPostById(post_id);
-    }
+	//게시물 단건 조회
+	@GetMapping("/{post_id}")
+	public PostFindDetailByIdResponseDto findPostByPostId(@PathVariable Long post_id) {
+		Post findPost = postService.findPostByPostId(post_id);
+		return new PostFindDetailByIdResponseDto(findPost.getTitle(), findPost.getContents(),
+			findPost.getUser().getUserName(), findPost.getUpdatedAt());
+	}
+
+	//게시글 좋아요 상태 토글
+	@PutMapping("/{post_id}/{user_id}/like")
+	public ResponseEntity<LikeNumResponseDto> toggleLikeStatus(@PathVariable Long post_id, @PathVariable Long user_id) {
+
+		Post post = postService.toggleLikeStatus(post_id, user_id);
+
+		return new ResponseEntity<>(new LikeNumResponseDto(post.getLikeCount()), HttpStatus.OK);
+	}
 }

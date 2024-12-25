d0bd776f909b7ad486eaa1eed1664bd90a94d25a
duol9 <lhyy0195@gmail.com>
2024-12-25 23:17:30 +0900

    Feat: 댓글 수정 비즈니스 로직 설계
    - postId와 commentId가 일치하는 데이터가 있는지 검사
    - 댓글 작성자 = 로그인 유저인지 확인 검사
    - 수정 된 댓글이 0개인지 검사
    - comment를 commentDto로 변환 후 반환

--- src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -1,6 +1,12 @@
 package org.example.newsfeed_project.comment.service;
 
+import org.example.newsfeed_project.comment.dto.CommentDto;
+import org.example.newsfeed_project.comment.dto.CommentRequestDto;
 import org.example.newsfeed_project.comment.repository.CommentRepository;
+import org.example.newsfeed_project.entity.Comment;
+import org.example.newsfeed_project.exception.ValidateException;
+import org.example.newsfeed_project.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -9,13 +15,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentService {
 
-	private CommentRepository commentRepository;
+	private final CommentRepository commentRepository;
+	private final UserRepository userRepository;
 
 	// 댓글 작성
 
 	// 댓글 조회
 
 	// 댓글 수정
+	public CommentDto modifyComment(Long loginUserId, Long postId, Long commetId, CommentRequestDto requestDto) {
+
+		// 해당 게시물에 댓글이 존재하는지 확인
+		Comment comment = commentRepository.findByCommentIdAndPostId(commetId, postId)
+			.orElseThrow(() -> new ValidateException("존재하지 않은 댓글입니다.", HttpStatus.NOT_FOUND));
+
+		// 본인 확인 (댓글 작성자 = 로그인 유저?)
+		if (!comment.getUser().getUserId().equals(loginUserId)) {
+			throw new ValidateException("본인만 수정 가능합니다.", HttpStatus.UNAUTHORIZED);
+		}
+
+		// 수정
+		int updatedRow = commentRepository.updateComment(commetId, requestDto.getComment());
+		if (updatedRow == 0) {
+			throw new ValidateException("댓글 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		return CommentDto.convertDto(comment);
+	}
 
 	// 댓글 삭제
 }

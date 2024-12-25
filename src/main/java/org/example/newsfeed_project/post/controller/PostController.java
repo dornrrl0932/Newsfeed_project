f075bfe322d92640fd0e0890a2369864b33d6790
임PRO스터\su992 <su9926@naver.com>
2024-12-25 20:29:27 +0900

    Feat: PostController 추가
    - PostController init 파일 추가

--- src/main/java/org/example/newsfeed_project/post/controller/PostController.java
+++ src/main/java/org/example/newsfeed_project/post/controller/PostController.java
@@ -1,7 +1,6 @@
 package org.example.newsfeed_project.post.controller;
 
 import org.example.newsfeed_project.post.service.PostService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

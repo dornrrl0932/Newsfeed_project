14800da663771e61fafdd83d5259c404562f1422
duol9 <lhyy0195@gmail.com>
2024-12-26 01:31:23 +0900

    Rename: 댓글requestDto 속성명 변경
    - comment -> comments
    - 통일성을 위해 변경

--- src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -1,22 +1,22 @@
 package org.example.newsfeed_project.comment.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.example.newsfeed_project.comment.dto.CommentDto;
 import org.example.newsfeed_project.comment.dto.CommentRequestDto;
 import org.example.newsfeed_project.comment.repository.CommentRepository;
 import org.example.newsfeed_project.entity.Comment;
-import org.example.newsfeed_project.exception.ValidateException;
-import org.example.newsfeed_project.user.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.example.newsfeed_project.entity.Post;
 import org.example.newsfeed_project.entity.User;
+import org.example.newsfeed_project.exception.ValidateException;
 import org.example.newsfeed_project.post.repository.PostRepository;
+import org.example.newsfeed_project.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -24,25 +24,25 @@ public class CommentService {
 
 	private final CommentRepository commentRepository;
 	private final UserRepository userRepository;
-    private final PostRepository postRepository;
+	private final PostRepository postRepository;
 
-    // 댓글 작성
-    public CommentDto saveComment(Long postId, Long userId, CommentRequestDto requestDto) {
-        Optional<Post> optionalPost = postRepository.findById(postId);
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if (optionalPost.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다.");
-        }
-        Post findPost = optionalPost.get();
-        User findUser = optionalUser.get();
+	// 댓글 작성
+	public CommentDto saveComment(Long postId, Long userId, CommentRequestDto requestDto) {
+		Optional<Post> optionalPost = postRepository.findById(postId);
+		Optional<User> optionalUser = userRepository.findById(userId);
+		if (optionalPost.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다.");
+		}
+		Post findPost = optionalPost.get();
+		User findUser = optionalUser.get();
 
+		Comment comment = new Comment(findPost, findUser, requestDto.getComments(), 0L, LocalDateTime.now());
+		comment = commentRepository.save(comment);
+		return new CommentDto(comment.getComments(), comment.getLike_count(), comment.getUser().getUserName(),
+			comment.getUpdatedAt());
+	}
 
-        Comment comment = new Comment(findPost, findUser,requestDto.getComment(), 0L, LocalDateTime.now());
-		comment =  commentRepository.save(comment);
-		return new CommentDto(comment.getComments(), comment.getLike_count(), comment.getUser().getUserName(), comment.getUpdatedAt());
-    }
-
-    // 댓글 조회
+	// 댓글 조회
 
 	// 댓글 수정
 	public CommentDto modifyComment(Long loginUserId, Long postId, Long commetId, CommentRequestDto requestDto) {
@@ -57,7 +57,7 @@ public class CommentService {
 		}
 
 		// 수정
-		int updatedRow = commentRepository.updateComment(commetId, requestDto.getComment());
+		int updatedRow = commentRepository.updateComment(commetId, requestDto.getComments());
 		if (updatedRow == 0) {
 			throw new ValidateException("댓글 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
 		}
@@ -65,5 +65,5 @@ public class CommentService {
 		return CommentDto.convertDto(comment);
 	}
 
-    // 댓글 삭제
+	// 댓글 삭제
 }

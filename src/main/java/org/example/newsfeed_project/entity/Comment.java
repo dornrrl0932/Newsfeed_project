83f735aaad0a6ac378bdb5499d39beaac9853257
duol9 <lhyy0195@gmail.com>
2024-12-25 22:37:32 +0900

    Rename: Comment id명 변경
    - 명확하게 나타내기 위해 commentId로 변경

--- src/main/java/org/example/newsfeed_project/entity/Comment.java
+++ src/main/java/org/example/newsfeed_project/entity/Comment.java
@@ -26,7 +26,7 @@ import lombok.Setter;
 public class Comment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long commentId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")

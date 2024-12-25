c3b12608a8faea973a14d772a31406f06ba74da4
임PRO스터\su992 <su9926@naver.com>
2024-12-26 01:10:23 +0900

    Feat: LikeNumResponseDto 추가
    - @AllArgsConstructor 어노테이션 추가

--- src/main/java/org/example/newsfeed_project/post/dto/LikeNumResponseDto.java
+++ src/main/java/org/example/newsfeed_project/post/dto/LikeNumResponseDto.java
@@ -1,8 +1,10 @@
 package org.example.newsfeed_project.post.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LikeNumResponseDto {
 
 	private Long LikeNum;

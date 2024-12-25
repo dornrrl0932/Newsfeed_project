9eac9441cdf1c5f32d65e79e35e2433ee0ef0ba9
duol9 <lhyy0195@gmail.com>
2024-12-25 22:45:21 +0900

    Feat: postId와 commentId가 일치한 데이터 조회하는 JPQL 구현
    - 이전 존재하는지 조회만 하는게 아닌 일치하는 데이터를 반환하는 쿼리메소드가 필요해 변경
    - 댓글id와 게시글id가 일치하는 데이터를 제공하는 쿼리메소드가 없어서 JPQL로 구현

--- src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
@@ -1,8 +1,17 @@
 package org.example.newsfeed_project.comment.repository;
 
+import java.util.Optional;
+
 import org.example.newsfeed_project.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	boolean existsCommentByPostPostId(Long postId);
+	@Query("SELECT c " +
+		"FROM Comment c " +
+		"WHERE c.commentId = :commentId " +
+		"AND c.post.postId = :postId"
+	)
+	Optional<Comment> findByCommentIdAndPostId(@Param("commentId") Long commentId, @Param("postId") Long postId);
 }

7c69993f17480a3afe4355873368a516e8b6935b
duol9 <lhyy0195@gmail.com>
2024-12-25 23:13:04 +0900

    Feat: 댓글 수정 JPQL 구현
    - commentId에 해당하는 댓글 수정
    - UPDATE 작업을 위해 @Modifying 어노테이션 사용
    - @Transactional을 기본으로 제공하지 않으므로 설정, 예외 발생 시 롤백

--- src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import org.example.newsfeed_project.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 	@Query("SELECT c " +
@@ -14,4 +16,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 		"AND c.post.postId = :postId"
 	)
 	Optional<Comment> findByCommentIdAndPostId(@Param("commentId") Long commentId, @Param("postId") Long postId);
+
+	@Modifying
+	@Transactional(rollbackFor = Exception.class)
+	@Query("UPDATE Comment c " +
+		"SET c.comments = :comments " +
+		"WHERE c.commentId = :commentId"
+	)
+	int updateComment(@Param("commentId") Long commentId, @Param("comments") String comments);
 }

2e588f72aafa34f6ed3f6f8063bb90863f02804a
duol9 <lhyy0195@gmail.com>
2024-12-26 09:01:23 +0900

    Feat: 댓글 request 입력 제한 추가
    - 250자 넘어갈 시 예외처리

--- src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
@@ -2,19 +2,20 @@ package org.example.newsfeed_project.comment.controller;
 
 import org.example.newsfeed_project.comment.dto.CommentDto;
 import org.example.newsfeed_project.comment.dto.CommentRequestDto;
-import jakarta.servlet.http.HttpServletRequest;
 import org.example.newsfeed_project.comment.service.CommentService;
 import org.example.newsfeed_project.user.session.SessionConst;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,25 +23,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final CommentService commentService;
+	private final CommentService commentService;
 
-    // 댓글 작성
-    @PostMapping("/")
-    public CommentDto saveComment(@PathVariable("post_id") Long postId,
-                                  @RequestBody CommentRequestDto requestDto,
-                                  HttpServletRequest request) {
+	// 댓글 작성
+	@PostMapping("/")
+	public CommentDto saveComment(@PathVariable("post_id") Long postId,
+		@Valid @RequestBody CommentRequestDto requestDto,
+		HttpServletRequest request) {
 
-        Long userId = (Long) request.getSession().getAttribute("loginUserId");
+		Long userId = (Long)request.getSession().getAttribute("loginUserId");
 		CommentDto commentDto = commentService.saveComment(postId, userId, requestDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(commentDto).getBody();
-    }
-    // 댓글 조회
+	}
+	// 댓글 조회
 
 	// 댓글 수정
 	@PatchMapping("/{comment_id}")
 	public ResponseEntity<CommentDto> modifyComment(@PathVariable(name = "post_id") Long postId,
 		@PathVariable(name = "comment_id") Long commentId,
-		@RequestBody CommentRequestDto dto, HttpServletRequest servletRequest) {
+		@Valid @RequestBody CommentRequestDto dto, HttpServletRequest servletRequest) {
 
 		HttpSession httpSession = servletRequest.getSession(false);
 		Long loginUserId = (Long)httpSession.getAttribute(SessionConst.LOGIN_USER_ID);
--- src/main/java/org/example/newsfeed_project/comment/dto/CommentRequestDto.java
+++ src/main/java/org/example/newsfeed_project/comment/dto/CommentRequestDto.java
@@ -1,10 +1,12 @@
 package org.example.newsfeed_project.comment.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CommentRequestDto {
+	@Size(max = 250, message = "댓글은 250자 이내로 입력 가능합니다.")
 	private String comments;
 }

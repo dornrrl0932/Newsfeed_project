75f0be4fdb03994fe8af20dafcdd1471b0ea831c
dornrrl0932 <dornrrl0932@naver.com>
2024-12-26 16:53:45 +0900

    Fix: 댓글 조회 API 수정

--- src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
@@ -37,5 +37,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 		return findById(commentId)
 			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."));
 	}
-
 }

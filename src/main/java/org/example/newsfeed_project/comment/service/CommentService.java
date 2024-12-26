a8d760a845d75ffd85faaec4506473b46c1e22bc
duol9 <lhyy0195@gmail.com>
2024-12-26 18:23:55 +0900

    Fix: 줄 위치 수정
    - 어노테이션과 import문이 섞여있는 것을 발견
    - 이로 인해 log를 쓸 수 없었으나 코드 정리 후 정상화 완료

--- src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -4,29 +4,27 @@ import java.util.Optional;
 
 import org.example.newsfeed_project.comment.dto.CommentDto;
 import org.example.newsfeed_project.comment.dto.CommentRequestDto;
-import jakarta.transaction.Transactional;
-import lombok.extern.slf4j.Slf4j;
 import org.example.newsfeed_project.comment.repository.CommentRepository;
+import org.example.newsfeed_project.comment.repository.CommetLikeRepository;
 import org.example.newsfeed_project.entity.Comment;
 import org.example.newsfeed_project.entity.CommentLike;
 import org.example.newsfeed_project.entity.Post;
 import org.example.newsfeed_project.entity.User;
+import org.example.newsfeed_project.exception.ValidateException;
 import org.example.newsfeed_project.post.repository.PostRepository;
 import org.example.newsfeed_project.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.example.newsfeed_project.comment.repository.CommetLikeRepository;
 import org.springframework.stereotype.Service;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.server.ResponseStatusException;
 
-
-@Slf4j
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -70,7 +68,6 @@ public class CommentService {
 					comment.getUser().getUserName(),
 					comment.getUpdatedAt()));
 	}
-	// 댓글 조회
 
 	// 댓글 수정
 	public CommentDto modifyComment(Long loginUserId, Long postId, Long commetId, CommentRequestDto requestDto) {
@@ -85,36 +82,34 @@ public class CommentService {
 		}
 
 		// 수정
-		int updatedRow = commentRepository.updateComment(commetId, requestDto.getComments());
-		if (updatedRow == 0) {
-			throw new ValidateException("댓글 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
-		}
+		commentRepository.updateComment(commetId, requestDto.getComments());
 
 		return CommentDto.convertDto(comment);
 	}
 
 	// 댓글 삭제
-    @Transactional
-    public void deleteComment(Long userId, Long postId, Long commentId) {
-        User user = userRepository.findUserByUserIdOrElseThrow(userId);
-        log.info("::: 게시물 조회 서비스가 동작하였습니다.");
-        Post post = postRepository.findPostByPostIdOrElseThrow(postId);
-        log.info("::: 댓글 조회 서비스가 동작하였습니다.");
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
-        if(userId != comment.getUser().getUserId()) {
-            throw new ValidateException("댓글 작성자가 아닙니다.", HttpStatus.UNAUTHORIZED);
-        }
-        commentRepository.deleteById(commentId);
-    }
+	@Transactional
+	public void deleteComment(Long userId, Long postId, Long commentId) {
+		User user = userRepository.findUserByUserIdOrElseThrow(userId);
+		log.info("::: 게시물 조회 서비스가 동작하였습니다.");
+		Post post = postRepository.findPostByPostIdOrElseThrow(postId);
+		log.info("::: 댓글 조회 서비스가 동작하였습니다.");
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+		if (userId != comment.getUser().getUserId()) {
+			throw new ValidateException("댓글 작성자가 아닙니다.", HttpStatus.UNAUTHORIZED);
+		}
+		commentRepository.deleteById(commentId);
+	}
+
 	// 댓글 좋아요 상태 토글
 	@Transactional
 	public Comment toggleCommentLikeSatus(Long commentId, Long userId) {
-		Comment findComment=commentRepository.findByCommentIdOrElseThrow(commentId);
+		Comment findComment = commentRepository.findByCommentIdOrElseThrow(commentId);
 		User findUser = userRepository.findUserByUserIdOrElseThrow(userId); //게시물에 좋아요를 누르려는 회원 객체
 
 		// 좋아요를 누르려는 사람=댓글 작성자 본인인 경우
-		if(findComment.getUser().getUserId().equals(userId)) {
+		if (findComment.getUser().getUserId().equals(userId)) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "본인의 댓글에는 좋아요를 누를 수 없습니다.");
 		}
 
@@ -122,21 +117,21 @@ public class CommentService {
 		Optional<CommentLike> optionalCommentLike = commetLikeRepository.findByCommentIdAndUserId(commentId, userId);
 
 		//좋아요를 누른 기록이 없는 경우, 새로운 PostLike 객체 생성 및 DB에 저장
-		if(optionalCommentLike.isEmpty()){
+		if (optionalCommentLike.isEmpty()) {
 			CommentLike newCommentLike = CommentLike.builder()
 				.comment(findComment)
 				.user(findUser)
 				.build();
 			commetLikeRepository.save(newCommentLike);
-			findComment.setLikeCount(findComment.getLikeCount()+1);
+			findComment.setLikeCount(findComment.getLikeCount() + 1);
 		} else {
-			CommentLike commentLike=optionalCommentLike.get();
-			if(commentLike.getLikeStatus()){
+			CommentLike commentLike = optionalCommentLike.get();
+			if (commentLike.getLikeStatus()) {
 				commentLike.setLikeStatus(false);
-				findComment.setLikeCount(findComment.getLikeCount()-1);
+				findComment.setLikeCount(findComment.getLikeCount() - 1);
 			} else {
 				commentLike.setLikeStatus(true);
-				findComment.setLikeCount(findComment.getLikeCount()+1);
+				findComment.setLikeCount(findComment.getLikeCount() + 1);
 			}
 			commetLikeRepository.save(commentLike);
 		}

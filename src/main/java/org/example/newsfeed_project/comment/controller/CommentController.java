15bffbb5b15e95e0066b39e0cdaa167124541ea3
dornrrl0932 <dornrrl0932@naver.com>
2024-12-26 14:32:52 +0900

    Fix: 댓글 조회 API 수정
    - @GetMapping -> @GetMapping("/")

--- src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
@@ -6,18 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.example.newsfeed_project.comment.dto.CommentRequestDto;
-import jakarta.servlet.http.HttpServletRequest;
-import org.example.newsfeed_project.user.session.SessionConst;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,16 +20,9 @@ public class CommentController {
 	private final CommentService commentService;
 
 	// 댓글 작성
-    private final CommentService commentService;
-
-    // 댓글 작성
-    @PostMapping("/")
-    public CommentDto saveComment(@PathVariable("post_id") Long postId,
-                                  @RequestBody CommentRequestDto requestDto,
-                                  HttpServletRequest request) {
 
 	// 댓글 조회
-	@GetMapping
+	@GetMapping("/")
 	public ResponseEntity<Page<CommentDto>> findcomment(@PathVariable Long postId,
 		@RequestParam(defaultValue = "1") int pageNum) {
 
@@ -45,23 +30,8 @@ public class CommentController {
 
 		return ResponseEntity.ok(commentPage);
 	}
-        Long userId = (Long) request.getSession().getAttribute("loginUserId");
-		CommentDto commentDto = commentService.saveComment(postId, userId, requestDto);
-		return ResponseEntity.status(HttpStatus.CREATED).body(commentDto).getBody();
-    }
-    // 댓글 조회
 
 	// 댓글 수정
-	@PatchMapping("/{comment_id}")
-	public ResponseEntity<CommentDto> modifyComment(@PathVariable(name = "post_id") Long postId,
-		@PathVariable(name = "comment_id") Long commentId,
-		@RequestBody CommentRequestDto dto, HttpServletRequest servletRequest) {
-
-		HttpSession httpSession = servletRequest.getSession(false);
-		Long loginUserId = (Long)httpSession.getAttribute(SessionConst.LOGIN_USER_ID);
-
-		return ResponseEntity.ok(commentService.modifyComment(loginUserId, postId, commentId, dto));
-	}
 
 	// 댓글 삭제
 
--- src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeed_project/comment/repository/CommentRepository.java
@@ -1,33 +1,13 @@
 package org.example.newsfeed_project.comment.repository;
 
-import java.util.Optional;
-
 import org.example.newsfeed_project.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	// 설정한 페이징 조건으로 댓글 조회
 	Page<Comment> findByPost_PostId(long postId, Pageable pageable);
-}
-	@Query("SELECT c " +
-		"FROM Comment c " +
-		"WHERE c.commentId = :commentId " +
-		"AND c.post.postId = :postId"
-	)
-	Optional<Comment> findByCommentIdAndPostId(@Param("commentId") Long commentId, @Param("postId") Long postId);
 
-	@Modifying
-	@Transactional(rollbackFor = Exception.class)
-	@Query("UPDATE Comment c " +
-		"SET c.comments = :comments " +
-		"WHERE c.commentId = :commentId"
-	)
-	int updateComment(@Param("commentId") Long commentId, @Param("comments") String comments);
 }

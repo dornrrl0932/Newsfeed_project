cf5ec1b6a2cee09b7caaf872fc8a3c20c4f765fe
임PRO스터\su992 <su9926@naver.com>
2024-12-26 14:32:21 +0900

    Feat: CommentLikeRepository 추가
    - id 기반 CommentLike 객체 조회 메서드 추가(findByCommentIdAndUserId())

---
+++ src/main/java/org/example/newsfeed_project/user/repository/CommetLikeRepository.java
@@ -0,0 +1,14 @@
+package org.example.newsfeed_project.user.repository;
+
+import java.util.Optional;
+
+import org.example.newsfeed_project.entity.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CommetLikeRepository extends JpaRepository<CommentLike, Long> {
+
+	@Query("select c from CommentLike c where c.comment.commentId=:commentId and c.user.userId=:userId")
+	Optional<CommentLike> findByCommentIdAndUserId(@Param("commentId") Long commentId, @Param("usrId") Long userId);
+}

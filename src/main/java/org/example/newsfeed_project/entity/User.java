9b729272f2503bb104aa92a2977c08edf6340a65
임PRO스터\su992 <su9926@naver.com>
2024-12-26 10:02:03 +0900

    Fix: User 생성자 수정
    - userName 오탈자 수정 (userNamem -> userName)
    - 생성자에 introduction 추가

--- src/main/java/org/example/newsfeed_project/entity/User.java
+++ src/main/java/org/example/newsfeed_project/entity/User.java
@@ -6,13 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Entity
 @Table(name = "user")
+@NoArgsConstructor
 public class User {
 
 	@Id
@@ -37,14 +39,11 @@ public class User {
 	@Setter
 	private Boolean status = true;
 
-	public User() {
-
-	}
-
-	public User(String email, String password, String userName) {
+	public User(String email, String password, String userName, String introduction) {
 		this.email = email;
 		this.password = password;
 		this.userName = userName;
+		this.introduction=introduction;
 	}
 
 	public User(String email, String password, String userName) {

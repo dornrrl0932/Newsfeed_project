aa1d836206f7462044d5f4274c6ec67378887114
dornrrl0932 <dornrrl0932@naver.com>
2024-12-26 16:10:51 +0900

    Fix: 댓글 조회 API 수정

--- src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed_project/comment/controller/CommentController.java
@@ -1,25 +1,15 @@
 package org.example.newsfeed_project.comment.controller;
 
 import org.example.newsfeed_project.comment.dto.CommentDto;
-import org.example.newsfeed_project.comment.dto.CommentRequestDto;
 import org.example.newsfeed_project.comment.service.CommentService;
-import org.example.newsfeed_project.user.session.SessionConst;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,15 +20,6 @@ public class CommentController {
 	private final CommentService commentService;
 
 	// 댓글 작성
-	@PostMapping("/")
-	public CommentDto saveComment(@PathVariable("post_id") Long postId,
-		@Valid @RequestBody CommentRequestDto requestDto,
-		HttpServletRequest request) {
-
-		Long userId = (Long)request.getSession().getAttribute("loginUserId");
-		CommentDto commentDto = commentService.saveComment(postId, userId, requestDto);
-		return ResponseEntity.status(HttpStatus.CREATED).body(commentDto).getBody();
-	}
 
 	// 댓글 조회
 	@GetMapping("/")
@@ -51,26 +32,9 @@ public class CommentController {
 	}
 
 	// 댓글 수정
-	@PatchMapping("/{comment_id}")
-	public ResponseEntity<CommentDto> modifyComment(@PathVariable(name = "post_id") Long postId,
-		@PathVariable(name = "comment_id") Long commentId,
-		@Valid @RequestBody CommentRequestDto dto, HttpServletRequest servletRequest) {
-
-		HttpSession httpSession = servletRequest.getSession(false);
-		Long loginUserId = (Long)httpSession.getAttribute(SessionConst.LOGIN_USER_ID);
-
-		return ResponseEntity.ok(commentService.modifyComment(loginUserId, postId, commentId, dto));
-	}
 
 	// 댓글 삭제
-	@DeleteMapping("/{comment_id}")
-	public ResponseEntity<String> deleteComment(HttpServletRequest request, @PathVariable(name = "post_id") Long postId,
-		@PathVariable(name = "comment_id") Long commentId) {
-		HttpSession session = request.getSession();
-		Long userId = (Long)session.getAttribute(SessionConst.LOGIN_USER_ID);
-		commentService.deleteComment(userId, postId, commentId);
-		return ResponseEntity.ok("댓글이 삭제 되었습니다.");
-	}
+
 	// 댓글 좋아요 상태 변경
 
 }
--- src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -1,14 +1,8 @@
 package org.example.newsfeed_project.comment.service;
 
-import java.util.Optional;
 import org.example.newsfeed_project.comment.dto.CommentDto;
-import org.example.newsfeed_project.comment.dto.CommentRequestDto;
-import jakarta.transaction.Transactional;
-import lombok.extern.slf4j.Slf4j;
 import org.example.newsfeed_project.comment.repository.CommentRepository;
-import org.example.newsfeed_project.entity.Comment;
 import org.example.newsfeed_project.entity.Post;
-import org.example.newsfeed_project.entity.User;
 import org.example.newsfeed_project.exception.ValidateException;
 import org.example.newsfeed_project.post.repository.PostRepository;
 import org.springframework.data.domain.Page;
@@ -16,33 +10,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
 import lombok.RequiredArgsConstructor;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
-	private final CommentRepository commentRepository;
-	private final UserRepository userRepository;
 	private final PostRepository postRepository;
+	private final CommentRepository commentRepository;
 
-    // 댓글 작성
-    public CommentDto saveComment(Long postId, Long userId, CommentRequestDto requestDto) {
-        Optional<Post> optionalPost = postRepository.findById(postId);
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if (optionalPost.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다.");
-        }
-        Post findPost = optionalPost.get();
-        User findUser = optionalUser.get();
-
-		Comment comment = new Comment(findPost, findUser, requestDto.getComments(), 0L);
-		comment = commentRepository.save(comment);
-		return new CommentDto(comment.getComments(), comment.getLike_count(), comment.getUser().getUserName(),
-			comment.getUpdatedAt());
-	}
+	// 댓글 작성
 
 	// 댓글 조회
 	public Page<CommentDto> findcomment(Long postId, int pageNum) {
@@ -65,38 +43,6 @@ public class CommentService {
 	// 댓글 조회
 
 	// 댓글 수정
-	public CommentDto modifyComment(Long loginUserId, Long postId, Long commetId, CommentRequestDto requestDto) {
-
-		// 해당 게시물에 댓글이 존재하는지 확인
-		Comment comment = commentRepository.findByCommentIdAndPostId(commetId, postId)
-			.orElseThrow(() -> new ValidateException("존재하지 않은 댓글입니다.", HttpStatus.NOT_FOUND));
-
-		// 본인 확인 (댓글 작성자 = 로그인 유저?)
-		if (!comment.getUser().getUserId().equals(loginUserId)) {
-			throw new ValidateException("본인만 수정 가능합니다.", HttpStatus.UNAUTHORIZED);
-		}
-
-		// 수정
-		int updatedRow = commentRepository.updateComment(commetId, requestDto.getComments());
-		if (updatedRow == 0) {
-			throw new ValidateException("댓글 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
-		}
-
-		return CommentDto.convertDto(comment);
-	}
 
 	// 댓글 삭제
-	@Transactional
-	public void deleteComment(Long userId, Long postId, Long commentId) {
-		User user = userRepository.findUserByUserIdOrElseThrow(userId);
-		log.info("::: 게시물 조회 서비스가 동작하였습니다.");
-		Post post = postRepository.findPostByPostIdOrElseThrow(postId);
-		log.info("::: 댓글 조회 서비스가 동작하였습니다.");
-		Comment comment = commentRepository.findById(commentId)
-				.orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
-		if(userId != comment.getUser().getUserId()) {
-			throw new ValidateException("댓글 작성자가 아닙니다.", HttpStatus.UNAUTHORIZED);
-		}
-		commentRepository.deleteById(commentId);
-	}
 }

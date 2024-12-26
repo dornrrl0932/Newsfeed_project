f76a4f9dd332ba9deed31d53a7ad9376ddd8f04e
임PRO스터\su992 <su9926@naver.com>
2024-12-26 14:30:17 +0900

    Feat: CommentService 추가
    - 좋아요 상태 토글 메서드 추가(toggleCommentLikeStatus())

--- src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed_project/comment/service/CommentService.java
@@ -7,15 +7,18 @@ import org.example.newsfeed_project.comment.dto.CommentDto;
 import org.example.newsfeed_project.comment.dto.CommentRequestDto;
 import org.example.newsfeed_project.comment.repository.CommentRepository;
 import org.example.newsfeed_project.entity.Comment;
+import org.example.newsfeed_project.entity.CommentLike;
 import org.example.newsfeed_project.entity.Post;
 import org.example.newsfeed_project.entity.User;
 import org.example.newsfeed_project.exception.ValidateException;
 import org.example.newsfeed_project.post.repository.PostRepository;
+import org.example.newsfeed_project.user.repository.CommetLikeRepository;
 import org.example.newsfeed_project.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -25,6 +28,7 @@ public class CommentService {
 	private final CommentRepository commentRepository;
 	private final UserRepository userRepository;
 	private final PostRepository postRepository;
+	private final CommetLikeRepository commetLikeRepository;
 
 	// 댓글 작성
 	public CommentDto saveComment(Long postId, Long userId, CommentRequestDto requestDto) {
@@ -38,7 +42,7 @@ public class CommentService {
 
 		Comment comment = new Comment(findPost, findUser, requestDto.getComments(), 0L, LocalDateTime.now());
 		comment = commentRepository.save(comment);
-		return new CommentDto(comment.getComments(), comment.getLike_count(), comment.getUser().getUserName(),
+		return new CommentDto(comment.getComments(), comment.getLikeCount(), comment.getUser().getUserName(),
 			comment.getUpdatedAt());
 	}
 
@@ -66,4 +70,41 @@ public class CommentService {
 	}
 
 	// 댓글 삭제
+
+	// 댓글 좋아요 상태 토글
+	@Transactional
+	public Comment toggleCommentLikeSatus(Long commentId, Long userId) {
+		Comment findComment=commentRepository.findByCommentIdOrElseThrow(commentId);
+		User findUser = userRepository.findUserByUserIdOrElseThrow(userId); //게시물에 좋아요를 누르려는 회원 객체
+
+		// 좋아요를 누르려는 사람=댓글 작성자 본인인 경우
+		if(findComment.getUser().getUserId().equals(userId)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "본인의 댓글에는 좋아요를 누를 수 없습니다.");
+		}
+
+		// 좋아요 이력 조회
+		Optional<CommentLike> optionalCommentLike = commetLikeRepository.findByCommentIdAndUserId(commentId, userId);
+
+		//좋아요를 누른 기록이 없는 경우, 새로운 PostLike 객체 생성 및 DB에 저장
+		if(optionalCommentLike.isEmpty()){
+			CommentLike newCommentLike = CommentLike.builder()
+													.comment(findComment)
+													.user(findUser)
+													.build();
+			commetLikeRepository.save(newCommentLike);
+			findComment.setLikeCount(findComment.getLikeCount()+1);
+		} else {
+			CommentLike commentLike=optionalCommentLike.get();
+			if(commentLike.getLikeStatus()){
+				commentLike.setLikeStatus(false);
+				findComment.setLikeCount(findComment.getLikeCount()-1);
+			} else {
+				commentLike.setLikeStatus(true);
+				findComment.setLikeCount(findComment.getLikeCount()+1);
+			}
+			commetLikeRepository.save(commentLike);
+		}
+
+		return commentRepository.save(findComment);
+	}
 }
